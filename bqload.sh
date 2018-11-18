fbdd3682b98a12a1a9141b17477da66b9994abbe
Riikka Schroderus <schroderusriikka@gmail.com>
2018-11-18 16:25:38 +0200

    fix typo

--- bqload.sh
+++ bqload.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 for i in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18
 do
 bq load --autodetect madrid_pollution_dataset.madrid20$i gs://$GOOGLE_CLOUD_PROJECT-data/csvs_per_year/madrid20$i.csv
